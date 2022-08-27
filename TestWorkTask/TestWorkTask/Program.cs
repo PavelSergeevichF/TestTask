7e2505c21f5e47dca73c30656ba15e5bcd2862e5
PavelSergeevichF <40184203+PavelSergeevichF@users.noreply.github.com>
2022-08-27 22:30:44 +0300

    TestTask

---
+++ TestWorkTask/TestWorkTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using GeometryMath;
+
+namespace TestWorkTask
+{
+    class Program
+    {
+        public float Radius = 10;
+        public Triangle Long= new Triangle();
+        public float a = 8f;
+        public float b = 4f;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.SetLongTriangle(program.a, program.b);
+            float AreaСircle = MathematicalGeometry.CalculateAreaCircle(program.Radius);
+
+            Console.WriteLine($"Проверка работы расчета площади круга {program.CeckAriaCircle(program.Radius, AreaСircle)}");
+            Console.WriteLine($"Проверка работы расчета площади треугольника {program.CeckAriaTriangle(program.Long)}");
+            float a, b, c;
+            program.Long.GetTriangle(out a, out b, out c);
+            Console.WriteLine($"Проверка работы метода является ли треугольник прямоугольным {MathematicalGeometry.CheckingForRightTriangle(a, b, c)}");
+        }
+        private void SetLongTriangle( float a, float b)
+        {
+            Long.SetTriangle(a,b, (float)Math.Sqrt(a*a+b*b));
+        }
+        private bool CeckAriaCircle(float R, float Aria)
+        {
+            int answer1 = (int)(R * R * Math.PI * 100);
+            int answer2 = (int)(Aria * 100);
+            return answer1 == answer2 ? true : false;
+        }
+        private bool CeckAriaTriangle(Triangle triangle)
+        {
+            float a, b, c;
+            triangle.GetTriangle(out a,out b,out c);
+            int answer1 = (int)(a*b/2*100);
+            int answer2 = (int)(MathematicalGeometry.AreaTriangle(a, b, c)*100);
+            return ((answer1 - answer2) > -2 && (answer1 - answer2) < 2) ? true : false;
+        }
+
+    }
+    struct Triangle
+    {
+        private float _a;
+        private float _b;
+        private float _c;
+        public void SetTriangle(float a, float b, float c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+        public void GetTriangle(out float a, out float b, out float c)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+    }
+}
---
+++ lib/Geometry/Geometry/MathematicalGeometry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GeometryMath
+{
+    public static class MathematicalGeometry
+    {
+        public static float CalculateAreaCircle( float radius)
+        {
+            return radius*radius*(float)Math.PI; 
+        }
+        public static float AreaTriangle(float a, float b, float c)
+        {
+            float halfPerimeter = (a+b+c) / 2;
+            return (float)Math.Sqrt(halfPerimeter*(halfPerimeter-a) *(halfPerimeter-b) *(halfPerimeter-c));
+        }
+        public static bool CheckingForRightTriangle(float a, float b, float c)
+        {
+            bool answer = false;
+            float halfPerimeter = (a + b + c) / 2;
+            float Area = (float)(Math.Sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c))*100);
+            float[] AreaVar =
+            {
+                100*a * b / 2,
+                100*b * c / 2,
+                100*c * a / 2
+            };
+            foreach(var answerArea in AreaVar)
+            {
+                int result1 = (int)Area;
+                int result2 = (int)answerArea;
+                if ((result1 - result2) > -2 && (result1 - result2) < 2)
+                {
+                    answer = true;
+                }
+            }
+            return answer;
+        }
+    }
+}
